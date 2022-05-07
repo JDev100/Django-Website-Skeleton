e0aa8b0b8d7b16fafb4d5c75f746499aa4d439fc
DaMan <jcazdev100@protonmail.com>
2022-05-06 21:23:38 -0700

    Basic Site Done
    Working towards adding Bootstrap

---
+++ website/main/urls.py
@@ -0,0 +1,11 @@
+from venv import create
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    #pick view based on path
+    path("<int:id>", views.index, name="list"),
+    path("home/", views.home, name = "home"),    
+    path("create/", views.create, name = "create")    
+]
