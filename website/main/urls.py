9721bb44cc6e972721215e3731c0a7171451c8b7
DaMan <jcazdev100@protonmail.com>
2022-05-07 00:03:09 -0700

    All Set

--- website/main/urls.py
+++ website/main/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     #pick view based on path
     path("<int:id>", views.index, name="list"),
+    path("home/", views.home, name = "home"),    
     path("", views.home, name = "home"),    
+    path("view/", views.view, name = "view"),    
     path("create/", views.create, name = "create")    
 ]
