14a2d1cc49722c10dcf49e279d74527dca67d1cc
DaMan <jcazdev100@protonmail.com>
2022-05-06 22:43:21 -0700

    Registering Set Up Made

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 urlpatterns = [
     #Pick proper url based on path put into search
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
+    path('register', v.register, name = "register")
 ]
